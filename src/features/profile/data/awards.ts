7b70b9055fad14dc40ad92d35e740732dffc6dca
dinesh_dk007 <dineshkumaar9025067808@gmail.com>
2025-05-26 11:11:45 +0530

    Changes in awards

--- src/features/profile/data/awards.ts
+++ src/features/profile/data/awards.ts
@@ -3,12 +3,30 @@ import { Award } from "../types/awards";
 export const AWARDS: Award[] = [
   {
     id: "a144bd19-3706-4e4c-ba22-0e0d8302642a",
+    prize: "AIR - 99",
+    title: "Unstop Coding Challenge",
+    date: "2025-04",
+    grade: "2nd Year",
+    description:
+      "- Field: Coding \n- Description: To solve competitve coding problems \n- Programming Language used: Java",
+  },
+  {
+    id: "a144bd19-3706-4e4c-ba22-0e0d8302642b",
     prize: "2nd Prize",
     title: "Duo Devs contest at Cognebula'25",
     date: "2025-02",
     grade: "2nd Year",
     description:
       "- Field: Coding \n- Description: To solve problems as a team\n- Programming Language used: Java",
+  },
+  {
+    id: "a144bd19-3706-4e4c-ba22-0e0d8302642x",
+    prize: "Higginbothams Voucher worth Rs. 2000",
+    title: "University Exam Topper 2023 - 2024",
+    date: "2025-04",
+    grade: "2nd Year",
+    description:
+      "- Field: Exam \n- Description:Achieved excellent academic performance in university examinations.  \n- Top score: Secured an 'O' grade (Outstanding) in the Software Development Practices examination, reflecting top-tier performance.",
   },
 
 ];
