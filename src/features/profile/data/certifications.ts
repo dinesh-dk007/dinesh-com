04e3469131beb89c441a2fe2257782639ae27702
dinesh-dk007 <dineshkumaar9025067808@gmail.com>
2025-06-19 10:45:52 +0530

    Certs Logo

--- src/features/profile/data/certifications.ts
+++ src/features/profile/data/certifications.ts
@@ -2,44 +2,27 @@ import { Certification } from "../types/certifications";
 
 export const CERTIFICATIONS: Certification[] = [
   {
-    title: "Next.js SEO Fundamentals",
-    issuer: "Vercel",
-    issuerLogo: "/images/companies/vercel.webp",
-    issueDate: "2025-04-26",
-    credentialID: "seo-47463-1745634751873",
-    credentialURL:
-      "https://nextjs.org/learn/certificate?course=seo&user=47463&certId=seo-47463-1745634751873",
-  },
-  {
-    title: "Next.js App Router Fundamentals",
-    issuer: "Vercel",
-    issuerLogo: "/images/companies/vercel.webp",
-    issueDate: "2025-04-26",
-    credentialID: "dashboard-app-47463-1745633258744",
-    credentialURL:
-      "https://nextjs.org/learn/certificate?course=dashboard-app&user=47463&certId=dashboard-app-47463-1745633258744",
-  },
-  {
     title: "React Foundations for Next.js",
     issuer: "Vercel",
-    issuerLogo: "/images/companies/vercel.webp",
+    issuerIconName: "vercel",
     issueDate: "2025-05-24",
     credentialID: "react-foundations-63266-1748106104371",
     credentialURL:
       "https://nextjs.org/learn/certificate?course=react-foundations&user=63266&certId=react-foundations-63266-1748106104371",
   },
-  {
+  /*{
     title: "Vietnamese Standardized Test of English Proficiency (CEFR B1)",
     issuer: "USSH-VNUHCM",
-    issuerLogo: "/images/companies/ussh-vnuhcm.webp",
+    issuerLogoURL: "/images/companies/ussh-vnuhcm.webp",
     issueDate: "2024-12-17",
     credentialID: "QH58202305187",
     credentialURL:
       "https://vbcc.hcmussh.edu.vn/?type=11&certificateType=7&no=QH58202305187&fullName=Nguy%E1%BB%85n+Ch%C3%A1nh+%C4%90%E1%BA%A1i&dob=2000-08-14",
-  },
+  },*/
   {
     title: "Google Cloud Computing Foundation",
-    issuer: "Google",
+    issuer: "Google Cloud Platform",
+    issuerIconName: "google",
     issueDate: "2025-03-22",
     credentialID: "6757f5a6-637a-4865-b902-09e6a3c36e78",
     credentialURL: "https://www.credly.com/badges/6757f5a6-637a-4865-b902-09e6a3c36e78/linked_in_profile",
@@ -47,6 +30,7 @@ export const CERTIFICATIONS: Certification[] = [
   {
     title: "Introduction to Binary Trees & Sorting",
     issuer: "Skillsoft",
+    issuerIconName:"skillsoft",
     issueDate: "2024-05-03",
     credentialID: "102882398",
     credentialURL: "https://skillsoft.digitalbadges.skillsoft.com/10ad7ce5-61b1-43dc-a104-a4c526193e17",
@@ -54,6 +38,7 @@ export const CERTIFICATIONS: Certification[] = [
   {
     title: "Critical Thinking for Better Judgment and Decision-Making",
     issuer: "LinkedIn",
+    issuerIconName:"LinkedIn",
     issueDate: "2024-05-05",
     credentialID: "ac6cf7af966772244c37911a77b7be72de7a6181a7f7524d59b1c701a9706208",
     credentialURL: "https://www.linkedin.com/learning/certificates/ac6cf7af966772244c37911a77b7be72de7a6181a7f7524d59b1c701a9706208",
@@ -61,6 +46,7 @@ export const CERTIFICATIONS: Certification[] = [
   {
     title: "Digital Skills: Digital Marketing",
     issuer: "Accenture",
+    issuerIconName: "accenture",
     issueDate: "2024-07-06",
     credentialID: "j9brlx5",
     credentialURL: "https://www.futurelearn.com/certificates/j9brlx5",
