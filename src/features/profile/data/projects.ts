825ff15335b5abb07f57ad674080d32711d6a9e5
dinesh-dk007 <dineshkumaar9025067808@gmail.com>
2026-07-22 12:47:31 +0530

    update: Projects

--- src/features/profile/data/projects.ts
+++ src/features/profile/data/projects.ts
@@ -2,6 +2,22 @@ import { Project } from "../types/projects";
 
 export const PROJECTS: Project[] = [
   {
+    id: "CodeSync",
+    title: "CodeSync - A Collaborative Code Editor",
+    time: "01.2026 - 03.2026",
+    link: "https://github.com/dinesh-dk007/CodeSync",
+    tags: [
+      "Clerk Authentication",
+      "Convex Database",
+      "Monako",
+      "React",
+      "NextJS",
+      "TailwindCSS",
+    ],
+    description:
+      "CodeSync is a real-time coding interview platform with a collaborative in-browser IDE and problem-solving workspace, simulating live technical interview environments for remote assessments.\n\n Implemented live screen sharing feature and session-based rooms using modern full-stack technologies, enabling multiple users to code and interact seamlessly in realtime.",
+  },
+  {
     id: "Pulse-Prep",
     title: "AI-Powered CRM Interaction Summarizer",
     time: "05.2025 - 06.2025",
@@ -14,6 +30,22 @@ export const PROJECTS: Project[] = [
     ],
     description:
       "Pulse-Prep is an AI-powered CRM note summarizer that extracts and condenses key information from customer interactions—such as calls, meetings, and notes—into clear, actionable summaries. Designed to boost productivity, it enables sales and support teams to quickly understand client history and make informed decisions without reading through lengthy CRM logs.\n\n“From raw CRM data to sharp summaries — instantly.”",
+  },
+  {
+    id: "Recipe Api Project",
+    title: "Recipe Api Project - Parse a File and create a Recipe API",
+    time: "01.2026 - 02.2026",
+    link: "https://github.com/dinesh-dk007/Recipe-Api-Project.git",
+    tags: [
+      "Java",
+      "Spring Boot",
+      "PostgreSQL",
+      "JPA",
+      "Git",
+      "Restful API",
+    ],
+    description:
+      "A Spring Boot REST API that parses recipe data from a JSON file, stores it in a database, and provides endpoints to manage and query recipes.\n\n Implemented file parsing, database integration, and RESTful API endpoints to manage recipe data efficiently.",
   },
   {
     id: "dinesh.com",
